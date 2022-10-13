3ff064610875fb65619e27eed4daba51f5e0a3a1
Winston Tang <winstontang@Winstons-MBP.attlocal.net>
2022-10-13 16:54:33 -0700

    feat: added /drinks endpoint

--- server.js
+++ server.js
@@ -1,6 +1,47 @@
 const express = require('express') // importing express library into the file (require = import)
 const app = express(); // creating 1st applicataion instance
-const drinks = [] // creating empty array called 'drinks'
+const drinks = [  // creating empty array called 'drinks'
+    {
+        name: 'cruddy mary',
+        price: 132,
+        image: 'https://i.imgur.com/Va5iIw5.jpg',
+      },
+      {
+        name: 'index on the beach',
+        price: 68,
+        image: 'https://i.imgur.com/XV2aPa2.jpg',
+      },
+      {
+        name: 'hack & coke',
+        price: 1,
+        image: 'https://i.imgur.com/rLOXFRI.jpg',
+      },
+      {
+        name: 'whiskey-value pair',
+        price: 11,
+        image: 'https://i.imgur.com/a7rmkoS.jpg',
+      },
+      {
+        name: '404 horsemen',
+        price: 202,
+        image: 'https://i.imgur.com/FLucOLr.jpg',
+      },
+      {
+        name: 'if stateMint julep',
+        price: 2,
+        image: 'https://i.imgur.com/yaoK0Dg.jpg',
+      },
+      {
+        name: 'APIPA',
+        price: 43,
+        image: 'https://i.imgur.com/qAhA7pi.jpg',
+      },
+      {
+        name: 'node to joy',
+        price: 56,
+        image: 'https://i.imgur.com/MbVdwZz.jpg',
+      },
+];
 
 /**
  * client -- server
@@ -13,7 +54,9 @@ app.get("/", (req, res) => {
     res.send("Welcome to the Gitpub App!");
 });
 
-
+app.get("/drinks", (req, res) => {
+    res.send(drinks);
+});
 
 app.listen(3000, () => { // app listens and starts server on port 3000
     console.log('listening');
